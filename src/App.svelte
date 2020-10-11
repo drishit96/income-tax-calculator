66fd1a8f52404a1b3ecc267611a49fb7bf739428
Drishit Mitra <drishitmitra@gmail.com>
2020-10-11 19:41:07 +0530

    Fix: Add Number constructor to number converter

--- src/App.svelte
+++ src/App.svelte
@@ -225,7 +225,7 @@
 		dialog.close();
 	}
 
-	function number(val) { return !val || isNaN(val) ? 0 : val; }
+	function number(val) { return !val || isNaN(val) ? 0 : Number(val); }
 </script>
 
 <style>
