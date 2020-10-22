fff1fd2b272e88d2ab07a16353f103b3e1546744
Drishit Mitra <drishitmitra@gmail.com>
2020-10-22 11:06:13 +0530

    Deferred font load

---
+++ public/loadFont.js
@@ -0,0 +1,6 @@
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://fonts.googleapis.com/css2?family=Inter&display=swap';
+link.type = 'text/css';
+var godefer = document.getElementsByTagName('link')[0];
+godefer.parentNode.insertBefore(link, godefer);
